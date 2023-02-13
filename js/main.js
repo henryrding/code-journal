68cb1e4941269e03b2685ebc0911eb0496171f84
Henry Ding <henryrding@yahoo.com>
2023-02-13 22:41:49 +0000

    Added 'submit' event listener to form element. #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 var $photoUrl = document.querySelector('#photo-url');
 var $photoPreview = document.querySelector('#photo-preview');
+var $form = document.querySelector('form');
 
 $photoUrl.addEventListener('input', function (event) {
   $photoPreview.setAttribute('src', event.target.value);
+});
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var formData = {
+    title: $form.elements.title.value,
+    photoUrl: $form.elements.photo.value,
+    notes: $form.elements.notes.value
+  };
+  formData.entryID = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(formData);
+  $photoPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 });
