cd19a3467b98ec2261ffc63bb7a75962281118e5
Henry Ding <henryrding@yahoo.com>
2023-02-14 18:02:42 +0000

    Created renderEntry function. #2

--- js/main.js
+++ js/main.js
@@ -19,3 +19,26 @@ $form.addEventListener('submit', function (event) {
   $photoPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 });
+
+function renderEntry(entry) {
+  var $entry = document.createElement('li');
+  $entry.className = 'row';
+  var $columnDiv = document.createElement('div');
+  $columnDiv.className = 'column-full column-half';
+  $entry.appendChild($columnDiv);
+  var $img = document.createElement('img');
+  $img.src = entry.photoUrl;
+  $columnDiv.appendChild($img);
+  var $columnDiv1 = document.createElement('div');
+  $columnDiv1.className = 'column-full column-half';
+  $entry.appendChild($columnDiv1);
+  var $h2 = document.createElement('h2');
+  $h2.textContent = entry.title;
+  $columnDiv1.appendChild($h2);
+  var $p = document.createElement('p');
+  $p.textContent = entry.notes;
+  $columnDiv1.appendChild($p);
+  return $entry;
+}
+
+renderEntry();
